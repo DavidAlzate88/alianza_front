3ce210eee26dec5e24601380619c6660c2ba9eb7
Juan Alzate <davids20xx@outlook.com>
2024-08-04 12:38:43 -0500

    create clients component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {NavComponent} from "./components/nav/nav.component";
+import {MatToolbar} from "@angular/material/toolbar";
+import {MatButton} from "@angular/material/button";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule, NavComponent, MatToolbar, MatButton],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'alianzaFront';
+  // displayedColumns:
+  //   string[] = ['key', 'id', 'email', 'phone', 'date'];
+  // dataSource = new MatTableDataSource(ELEMENT_DATA); // Reemplaza con tus datos reales
 }
+
+// Datos de ejemplo (reemplazar con datos reales)
+// const ELEMENT_DATA: any[] = [
+//   {key: '1', id: '123', email: 'ejemplo@email.com', phone: '123456789', date: '01/01/2024'},
+// ];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {ClientsComponent} from "./components/clients/clients.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: ClientsComponent,
+    title: 'Home page',
+  },
+  // {
+  //   path: 'details/:id',
+  //   component: DetailsComponent,
+  //   title: 'Home details',
+  // },
+];
